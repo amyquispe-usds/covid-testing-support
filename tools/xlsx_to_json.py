d477380c4316b809385f7293fc10dcf34b6b1a5c
Amy Quispe <Amy.M.Quispe@omb.eop.gov>
2021-12-21 21:04:16 -0800

    general script structure for parsing location information

---
+++ tools/xlsx_to_json.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from datetime import date
+import json
+import pandas
+import sys
+
+def process_location(labels, location, last):
+  row = 0
+  total_rows = len(labels.index) 
+  while row < total_rows:
+    label = labels.iloc[row]
+    attribute = location.iloc[row]
+    print("Label: {}, attribute: {}".format(label,attribute))
+    row += 1
+  if last:
+    return "{\"something\": \"somethingelse\"}"
+  return "{\"something\": \"somethingelse\"},"
+
+def process_df(df):
+  labels = df.iloc[:,1]
+  locations = df.iloc[:,2:]
+  today = date.today().strftime("%Y-%m-%d")
+  # TODO take in url as an argument
+  result = """
+         {{
+           "@context": "https://schema.org",
+           "@type": "SpecialAnnouncment",
+           "name": "COVID-19 Testing Facilities",
+           "datePosted": "{}",
+           "url": "",
+           "announcmentLocation": [
+         """.format(today)
+  number_of_locations = len(locations.columns)
+  location = 0
+  last = False
+  while location < number_of_locations:
+    if (location+1)==number_of_locations:
+      last = True 
+    location_blob = process_location(labels, locations.iloc[:,location], last)
+    result += location_blob
+    location += 1
+  result += """
+            ]
+          }"""
+  print("<script type=\"application/ld+json\">")
+  parsed = json.loads(result.strip())
+  print(json.dumps(parsed, indent=4))
+  print("</script>")
+
+def main():
+  if len(sys.argv) < 3:
+    print("Arguments missing. Run `./xlsx_to_json filname sheetname.`")
+    exit()
+  filename = sys.argv[1]
+  sheetname = sys.argv[2]
+  df = pandas.read_excel(filename, sheet_name=sheetname)
+  process_df(df)
+
+if __name__ == "__main__":
+  main()
