9ec2b9dad92d5851fb08287f343671c4febf7caa
Amy Quispe <Amy.M.Quispe@omb.eop.gov>
2021-12-22 11:02:35 -0800

    add attributes sketched out

--- tools/xlsx_to_json.py
+++ tools/xlsx_to_json.py
@@ -6,7 +6,78 @@ import pandas
 import sys
 
 def process_attribute(blob, label, attribute):
-  blob[label] = attribute
+  if "Name" == label: 
+    blob["name"] = attribute
+#  elif "Unique ID" == label:
+  elif "Address Line 1" == label:
+    blob["address"] = str(attribute)
+  elif "Address Line 2" == label:
+    blob["address"] += ", "
+    blob["address"] += str(attribute)
+  elif "City or Locality" == label:
+    blob["address"] += ", "
+    blob["address"] += str(attribute)
+  elif "State or Province" == label:
+    blob["address"] += ", "
+    blob["address"] += str(attribute)
+  elif "Postal Code" == label:
+    blob["address"] += ", "
+    blob["address"] += str(attribute)
+  elif "COVID-19 testing website" == label:
+    blob["url"] = str(attribute)
+#  elif "Free COVID-19 tests" == label:
+#  elif "Phone" == label:
+#  elif "Email" == label:
+#  elif "Website" == label:
+#  elif "Category" == label:
+#  elif "Latitude" == label:
+#  elif "Longitude" == label:
+#  elif "Monday" == label:
+#  elif "Tuesday" == label:
+#  elif "Wednesday" == label:
+#  elif "Thursday" == label:
+#  elif "Friday" == label:
+#  elif "Saturday" == label:
+#  elif "Sunday" == label:
+#  elif "Appointment required" == label:
+#  elif "Referral required" == label:
+#  elif "Patient restrictions" == label:
+#  elif "Location instructions" == label:
+#  elif "Insurance accepted" == label:
+#  elif "Insurance required" == label:
+#  elif "Minimum age" == label:
+#  elif "Languages spoken at location" == label:
+#  elif "Wheelchair accessible entrance" == label:
+#  elif "Wheelchair accessible parking" == label:
+#  elif "Wheelchair accessible restroom" == label:
+#  elif "Wheelchair accessible elevator" == label:
+#  elif "Non-English interpreter services" == label:
+#  elif "ASL interpreter services" == label:
+#  elif "Drive-thru options" == label:
+#  elif "Non-rapid PCR/NAAT test" == label:
+#  elif "Non-rapid PCR/NAAT cost" == label:
+#  elif "Non-rapid PCR/NAAT time to get results (hours)" == label:
+#  elif "Non-rapid PCR/NAAT test method" == label:
+#  elif "Antigen test" == label:
+#  elif "Antigen test cost" == label:
+#  elif "Antigen test time to get results (hours)" == label:
+#  elif "Antibody test" == label:
+#  elif "Antibody test cost" == label:
+#  elif "Antibody test time to get results (hours)" == label:
+#  elif "Rapid PCR/NAAT test" == label:
+#  elif "Rapid PCR/NAAT test cost" == label:
+#  elif "Rapid PCR/NAAT test time to get results (hours)" == label:
+#  elif "Rapid PCR/NAAT test method" == label:
+#  elif "At-home tests" == label:
+#  elif "At-home test cost" == label:
+#  elif "Mail-in test" == label:
+#  elif "Mail-in test cost" == label:
+#  elif "Mail-in test time to get results (hours)" == label:
+#  elif "Also a COVID-19 vaccination a location" == label:
+#  elif "COVID-19 vaccination webiste" == label:
+#  elif "Other Notes" == label:
+  else:
+    blob[label] = str(attribute)
 
 def process_location(labels, location, last):
   row = 0
@@ -21,7 +92,6 @@ def process_location(labels, location, last):
     process_attribute(location_blob, label, attribute)
     row += 1
   return_string = json.dumps(location_blob)
-  print(return_string)
   if not last:
     return_string += ","
   return return_string
@@ -52,10 +122,8 @@ def process_df(df):
   result += """
             ]
           }"""
-  print("<script type=\"application/ld+json\">")
   parsed = json.loads(result.strip())
-  print(json.dumps(parsed, indent=4))
-  print("</script>")
+  print("<script type=\"application/ld+json\">\n{}\n</script>".format(json.dumps(parsed,indent=4)))
 
 def main():
   if len(sys.argv) < 3:
